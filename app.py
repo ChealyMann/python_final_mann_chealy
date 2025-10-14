169271c4fbfcbdce8e46b3ddb0917297c0545358
MannChealy <Lychea664@gmail.com>
2025-10-14 21:47:59 +0700

    update form using wtform

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask
 from blueprints import nav
 from blueprints.nav import nav_bp
 from blueprints.products import product_bp
+from config import Config
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
     app.register_blueprint(nav_bp)
     app.register_blueprint(product_bp)
+
     return app
 
 
--- blueprints/nav.py
+++ blueprints/nav.py
@@ -1,13 +1,23 @@
 from flask import Blueprint , render_template , redirect , url_for
+
+from form.contact import ContactForm
+
 nav_bp = Blueprint('nav', __name__)
 
 @nav_bp.route('/')
 def home():
     return render_template('frontend/home.html')
 
-@nav_bp.route('/contact')
+@nav_bp.route('/contact',methods=['GET','POST'])
 def contact():
-    return render_template('frontend/pages/contact.html')
+    form = ContactForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        subject = form.subject.data
+        message = form.message.data
+
+    return render_template('frontend/pages/contact.html',form=form)
 
 @nav_bp.route('/shop')
 def shop():

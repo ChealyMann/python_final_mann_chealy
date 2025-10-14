37a97926fbc59a46361d323531cd32479e179e2a
MannChealy <Lychea664@gmail.com>
2025-10-14 20:59:18 +0700

    finish extends

--- blueprints/home.py
+++ blueprints/home.py
@@ -3,4 +3,8 @@ home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('frontend/home.html')
+
+@home_bp.route('/contact')
+def contact():
+    return render_template('frontend/pages/contact.html')

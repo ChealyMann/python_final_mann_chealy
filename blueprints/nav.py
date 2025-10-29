994dc2ab257666f4d2d17ba47c50d102f647477a
MannChealy <Lychea664@gmail.com>
2025-10-29 21:35:25 +0700

    before deploy

--- blueprints/nav.py
+++ blueprints/nav.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Blueprint , render_template , redirect , url_for
 
 from form.contact import ContactForm
@@ -8,7 +9,7 @@ nav_bp = Blueprint('nav', __name__)
 def home():
     return render_template('frontend/home.html')
 
-@nav_bp.route('/contact',methods=['GET','POST'])
+@nav_bp.route('/contact', methods=['GET', 'POST'])
 def contact():
     form = ContactForm()
     if form.validate_on_submit():
@@ -17,7 +18,35 @@ def contact():
         subject = form.subject.data
         message = form.message.data
 
-    return render_template('frontend/pages/contact.html',form=form)
+
+        token = '8257940589:AAGiMrNXXpHPP9YqMDG5e06fyjJaYo58nM0'
+
+
+        url = f"https://api.telegram.org/bot{token}/sendMessage"
+
+        payload = {
+            "chat_id": "1095872862",
+            "text": f"""
+NAME : {name}
+EMAIL : {email}
+SUBJECT : {subject}
+MESSAGE : {message}
+""",
+            "disable_web_page_preview": False,
+            "disable_notification": False
+        }
+
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json"
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+
+        return redirect(url_for('nav.home'))
+
+    return render_template('frontend/pages/contact.html', form=form)
+
 
 @nav_bp.route('/shop')
 def shop():
--- form/contact.py
+++ form/contact.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms.fields.simple import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Length
 
 
 class ContactForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired(),Length(min=2, max=50)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    subject = StringField('Subject', validators=[DataRequired()])
-    message = TextAreaField('Message', validators=[DataRequired()])
+    subject = StringField('Subject', validators=[DataRequired(),Length(min=2, max=50)])
+    message = TextAreaField('Message', validators=[DataRequired(),Length(min=2, max=500)])
     submit = SubmitField('Submit')
